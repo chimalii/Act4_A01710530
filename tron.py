70db965970141fb3690525d507b392a15696c0cb
Gaby Chimali Nava <chimalinava@gmail.com>
2024-09-12 12:00:34 -0600

    Fix code with flake 8

--- tron.py
+++ tron.py
@@ -8,7 +8,8 @@ Exercises
 4. How would you create a computer player?
 """
 
-from turtle import *
+from turtle import update, ontimer, setup, hideturtle
+from turtle import tracer, listen, onkey, done
 
 from freegames import square, vector
 
